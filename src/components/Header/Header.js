1237fdea913b28d74b6ceb218eb5f6e37b815da8
Senyth <33655880+ohsenyth@users.noreply.github.com>
2019-11-12 16:59:17 +0800

    added SASS and CSS Modules

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import './Header.scss';
 
 class Header extends React.Component {
     render() {
         return (
-            <h1>Hello, World</h1>
+            <div id="header">
+                <h1>Hello World</h1>
+            </div>
         )
     }
 }
